ee60b08cdb2b9fd84bf3665fc2d33cf975cee22e
Bach Tran <batr.schip@gmail.com>
2019-07-08 16:21:09 +0700

    spring-boot

--- itern-backend/src/main/java/net/itern/backend/PublicAnnouncement.java
+++ itern-backend/src/main/java/net/itern/backend/PublicAnnouncement.java
@@ -1,4 +1,15 @@
 package net.itern.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController("/")
 public class PublicAnnouncement {
+
+    @GetMapping
+    public String hello() {
+        return "System initialized at " + LocalTime.now();
+    }
 }
